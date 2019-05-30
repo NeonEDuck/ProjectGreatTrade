e16a4cc61df22abf1140dbc1055064ee30dfa987
AnnOLoveCat <43593864+AnnOLoveCat@users.noreply.github.com>
2019-05-30 12:37:35 +0800

    ..

--- routes/product_list_add.js
+++ routes/product_list_add.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-//接收GET請求
-router.get('/', function(req, res, next) {
-    res.render('product_list_add'); 
+//增加引用函式
+const product = require('./utility/product');
+
+//接收POST請求
+router.post('/', function(req, res) {
+    var prono = req.body.prono;                  //取得產品編號
+    var proname = req.body.proname;              //取得產品名稱
+    var price = Number(req.body.price);          //取得價格
+    var inventorydate = req.body.inventorydate;  //取得盤點日期
+    var propayment = req.body.propayment;  
+    var tel = Number(req.body.tel);
+    var address = req.body.address;
+
+    // 建立一個新資料物件
+    var newData={
+        prono:prono,
+        proname:proname,
+        price:price,
+        inventorydate:inventorydate,
+        propayment:propayment,
+        tel:tel,
+        address:address
+    } 
+    
+    product.add(newData).then(d => {
+        if (d==0){
+            res.render('addSuccess');  //傳至成功頁面
+        }else{
+            res.render('addFail');     //導向錯誤頁面
+        }  
+    })
 });
 
-module.exports = router; 
+module.exports = router;
